2836269231e05c38bf620691fb0e94ad6fa97a06
armin <armin536@gmail.com>
2021-04-03 00:45:23 +0430

    Initial Commit

---
+++ PersianSeeFood/PersianSeeFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersianSeeFoodApp.swift
+//  PersianSeeFood
+//
+//  Created by armin on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct PersianSeeFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
